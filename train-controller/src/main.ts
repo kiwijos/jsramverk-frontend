d5f26463b14075b765e975a058c9daf630f801b5
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-09-19 18:04:51 +0200

    Add prime, socket.io, maplibre

--- train-controller/src/main.ts
+++ train-controller/src/main.ts
@@ -1,9 +1,27 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Password from "primevue/password";
+import InputText from "primevue/inputtext";
+import TabMenu from "primevue/tabmenu";
+import Divider from "primevue/divider";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import MapComponent from "./components/MapComponent.vue";
 
 createApp(App)
+    .use(PrimeVue)
     .use(router)
-    .mount('#app')
+    .component("Button", Button)
+    .component("Password", Password)
+    .component("InputText", InputText)
+    .component("TabMenu", TabMenu)
+    .component("Divider", Divider)
+    .component("DataTable", DataTable)
+    .component("Column", Column)
+    .component("MapComponent", MapComponent)
+    .mount("#app");
