41ae847395b65583fc501fe6c6caa82bb274bad2
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-09-19 18:06:24 +0200

    Login component

--- train-controller/src/components/LoginComponent.vue
+++ train-controller/src/components/LoginComponent.vue
@@ -1,11 +1,17 @@
-<!--Basic login page in vue-->
 <template>
-    <div class="login">
-        <h1>Login</h1>
-        <form>
-            <input type="text" placeholder="Username" v-model="username" />
-            <input type="password" placeholder="Password" v-model="password" />
-            <input type="submit" value="Login" @click="login" />
+    <div>
+        <h1>Login/Logo</h1>
+        <Divider />
+        <form class="flex gap-3 mt-5">
+            <span class="p-float-label">
+                <InputText type="text" v-model="username" inputId="username" />
+                <label for="username">Username</label>
+            </span>
+            <span class="p-float-label">
+                <Password v-model="password" toggleMask inputId="password" :feedback="false" />
+                <label for="password">Password</label>
+            </span>
+            <Button label="Login" @click="login" />
         </form>
     </div>
 </template>
@@ -13,21 +19,30 @@
 <!-- Fake login script in vue-->
 <script lang="ts">
 export default {
-    name: 'LoginComponent',
+    name: "LoginComponent",
     data() {
         return {
-            username: '',
-            password: ''
-        }
+            username: "",
+            password: ""
+        };
     },
     methods: {
         login() {
-            if (this.username === 'admin' && this.password === 'admin') {
-                console.log('Logged in');
+            if (this.username === "admin" && this.password === "admin") {
+                console.log("Logged in");
+                this.$router.push("/");
             } else {
-                alert('Wrong credentials');
+                alert("Wrong credentials");
             }
         }
     }
-}
+};
 </script>
+
+<style scoped>
+.loginForm {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
