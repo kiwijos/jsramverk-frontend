31866bf31fda8d74aab3c4984dc953a303218c1a
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-09-21 17:30:33 +0200

    Create TrainService.ts

---
+++ train-controller/src/services/TrainService.ts
@@ -0,0 +1,19 @@
+import type { Ticket } from "@/models/Ticket.model";
+import type { TicketCode } from "@/models/TicketCode.model";
+import type { TrainDelay } from "@/models/TrainDelay.model";
+import axios from "axios";
+
+export default {
+    async getDelayedTrains(): Promise<TrainDelay[]> {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/delayed`);
+        return response.data.data;
+    },
+    async getTickets(): Promise<Ticket[]> {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/tickets`);
+        return response.data.data;
+    },
+    async getTicketCodes(): Promise<TicketCode[]> {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/codes`);
+        return response.data.data;
+    }
+}
