be2549aa02966ac3d2c119a00910ee4632865b99
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-09-21 17:29:53 +0200

    Ticket table view

--- train-controller/src/components/TicketComponent.vue
+++ train-controller/src/components/TicketComponent.vue
@@ -1,3 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Get tickets on mount
+import { ref, onMounted } from "vue";
+import type { Ticket } from "@/models/Ticket.model";
+import TrainService from "@/services/TrainService";
 
-<template></template>
+const tickets = ref<Ticket[]>([]);
+onMounted(async () => {
+    tickets.value = await TrainService.getTickets();
+});
+
+</script>
+
+<template>
+    <h1>Ärenden</h1>
+    <Divider />
+    <div class="flex gap-3">
+        <DataTable
+            :value="tickets"
+            :paginator="true"
+            :rows="10"
+            :rowsPerPageOptions="[10, 25, 50]"
+            :loading="tickets.length === 0"
+            :stripedRows="true"
+            class="w-5"
+        >
+            <Column field="code" header="Kod"></Column>
+            <Column field="trainnumber" header="Tåg"></Column>
+            <Column field="traindate" header="Datum"></Column>
+        </DataTable>
+    </div>
+</template>
