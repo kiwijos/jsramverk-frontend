62dd2ec6878140d24466e08ea514137962e9f4f7
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-09-27 14:57:13 +0200

    Basic test setup

---
+++ src/tests/DelayedComponent.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+import DelayedComponent from "../components/DelayedComponent.vue";
+import { mount } from "@vue/test-utils";
+
+test("DelayedComponent", async () => {
+    const wrapper = await mount(DelayedComponent, {
+        global: {
+            components: {
+                Divider: { template: "<div></div>" },
+                Column: { template: "<div></div>" },
+                Button: { template: "<div></div>" },
+                DataTable: { template: "<div></div>" },
+                MapComponent: { template: "<div></div>" },
+                Dropdown: { template: "<div></div>" },
+                Dialog: { template: "<div></div>" }
+            }
+        }
+    });
+    expect(wrapper.html()).toContain("Försenade tåg");
+});
