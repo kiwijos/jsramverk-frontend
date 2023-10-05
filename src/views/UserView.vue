e3f008bc448f0f164d2d7251b1f9fcb20b034867
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-10-05 13:50:17 +0200

    Name change and router-view

---
+++ src/views/UserView.vue
@@ -0,0 +1,17 @@
+<!--Import and use LoginComponent in script too-->
+<script setup lang="ts"></script>
+
+<template>
+    <div class="centering">
+        <router-view />
+    </div>
+</template>
+
+<style scoped>
+.centering {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
