7e2325ef004c598059daeb3f06804bef57931025
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-10-05 13:49:18 +0200

    Add token to requests

--- src/services/TrainService.ts
+++ src/services/TrainService.ts
@@ -6,19 +6,35 @@ import axios from "axios";
 
 export default {
     async getDelayedTrains(): Promise<TrainDelay[]> {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/delayed`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/delayed`, {
+            headers: {
+                "x-access-token": sessionStorage.getItem("x-access-token")
+            }
+        });
         return response.data.data;
     },
     async getTickets(): Promise<Ticket[]> {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/tickets`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/tickets`, {
+            headers: {
+                "x-access-token": sessionStorage.getItem("x-access-token")
+            }
+        });
         return response.data.data;
     },
     async getTicketCodes(): Promise<TicketCode[]> {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/codes`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/codes`, {
+            headers: {
+                "x-access-token": sessionStorage.getItem("x-access-token")
+            }
+        });
         return response.data.data;
     },
     async createTicket(request: TicketCreateDto): Promise<Ticket> {
-        const response = await axios.post(`${import.meta.env.VITE_API_URL}/tickets`, request);
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/tickets`, request, {
+            headers: {
+                "x-access-token": sessionStorage.getItem("x-access-token")
+            }
+        });
         return response.data.data;
     }
 };
