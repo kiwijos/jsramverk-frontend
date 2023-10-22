78d24f2058a7bc439436645d2da28cc970cb3858
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-10-22 15:12:28 +0200

    Add login before run tests
    Assume user kent for now

--- src/tests/TrainService.test.ts
+++ src/tests/TrainService.test.ts
@@ -4,24 +4,29 @@ import type { TicketCode } from "./../models/TicketCode.model";
 // Test calls to api using TrainService
 
 import TrainService from "../services/TrainService";
-import { expect, test } from "vitest";
+import { expect, test, beforeAll } from "vitest";
+import AuthService from "@/services/AuthService";
 
-test.skip("TrainService get ticket codes", async () => {
+beforeAll(async () => {
+    await AuthService.loginGrapQL("kent", "kent");
+});
+
+test("TrainService get ticket codes", async () => {
     const codes = await TrainService.getTicketCodes();
     expect(codes).toBeInstanceOf(Array<TicketCode>);
 });
 
-test.skip("TrainService get delayed trains", async () => {
+test("TrainService get delayed trains", async () => {
     const codes = await TrainService.getDelayedTrains();
     expect(codes).toBeInstanceOf(Array<TrainDelay>);
 });
 
-test.skip("TrainService get all tickets", async () => {
+test("TrainService get all tickets", async () => {
     const codes = await TrainService.getTickets();
     expect(codes).toBeInstanceOf(Array<Ticket>);
 });
 
-test.skip("TrainService create ticket", async () => {
+test("TrainService create ticket", async () => {
     const date = new Date();
     const ticket = await TrainService.createTicket({
         code: "ABC123",
