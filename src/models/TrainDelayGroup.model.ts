f13c96c054f5731cb1d3a4fbc42e78b4693d48d9
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-10-23 18:11:47 +0200

    Include station for to and from destinations

--- src/models/TrainDelayGroup.model.ts
+++ src/models/TrainDelayGroup.model.ts
@@ -1,6 +1,9 @@
 import type { TrainDelayWithStationDto } from "./TrainDelayWithStationDto.model";
+import type { TrainStation } from "./TrainStation.model";
 
 export interface TrainDelayGroup {
     id: string;
+    fromStation: TrainStation;
+    toStation: TrainStation;
     data: TrainDelayWithStationDto[];
 }
